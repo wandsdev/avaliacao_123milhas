356123acc45b0ecc422c89fc5400a99b894d8a36
wandsdev <wandsdeveloper@gmail.com>
2021-08-20 19:55:56 -0300

    criada rotina de agrupamento de voos

--- app/Services/FlightsService.php
+++ app/Services/FlightsService.php
@@ -4,62 +4,104 @@
 namespace App\Services;
 
 
+use Illuminate\Support\Str;
+
 class FlightsService
 {
+    /**
+     * @var int
+     */
+    public $cheapestPrice = 100000000000000000000000000000;
+
+    /**
+     * @var string
+     */
+    public $cheapestGroup = '';
+
+    /**
+     * @var array
+     */
+    public $groups = array();
+
+    /**
+     * @var array
+     */
+    public $flights = array();
+
+    /**
+     * @var
+     */
+    public $flightsAux = array();
+
     /**
      * @param $flights
      * @return array
      */
     public function flightsGrouping(array $flights): array
     {
-        $flightsAux = array();
+        $this->flights = $flights;
 
         foreach ($flights as $key => $value) {
             if ($value['outbound']) {
-                $flightsAux['outbound'][$value['fare']][$value['price']][] = $value;
+                $this->flightsAux['outbound'][$value['fare']][$value['price']][] = $value;
             } else {
-                $flightsAux['inbound'][$value['fare']][$value['price']][] = $value;
+                $this->flightsAux['inbound'][$value['fare']][$value['price']][] = $value;
             }
         }
 
-        $groupedFlights = $this->makeGroups($flightsAux);
-        return $this->makeResponse($flights, $groupedFlights);
+        $this->makeGroups();
+        return $this->makeResponse();
     }
 
-    /**
-     * @param array $flightsAux
-     * @return array
-     */
-    private function makeGroups(array $flightsAux): array
+    private function makeGroups()
     {
-        $groups = array();
-
-        foreach ($flightsAux['outbound'] as $keyO => $itemO) {
-            foreach ($flightsAux['inbound'] as $keyI => $itemI) {
+        foreach ($this->flightsAux['outbound'] as $keyO => $itemO) {
+            foreach ($this->flightsAux['inbound'] as $keyI => $itemI) {
                 if ($keyO == $keyI) {
                     foreach ($itemO as $o) {
                         foreach ($itemI as $i) {
-                            $groups[] = ['outbound' => $o, 'inbound' => $i];
+                            $uniqueId = $this->createUniqueIdgroup($o, $i);
+                            $price = $o[0]['price'] + $i[0]['price'];
+                            if ($this->cheapestPrice > $price) {
+                                $this->cheapestPrice = $price;
+                                $this->cheapestGroup = $uniqueId;
+                            }
+                            $this->groups[] = ['uniqueId' => $uniqueId, 'totalPrice' => $price, 'outbound' => $o, 'inbound' => $i];
                         }
                     }
                 }
             }
         }
-        return $groups;
     }
 
     /**
-     * @param array $flights
-     * @param array $groupedFlights
-     * @return array[]
+     * @return array
      */
-    private function makeResponse(array $flights, array $groupedFlights): array
+    private function makeResponse(): array
     {
 
         return array(
-            "flights" => $flights,
-            "groups" => $groupedFlights
+            "flights" => $this->flights,
+            "groups" => $this->groups,
+            "totalGroups" => count($this->groups),
+            "totalFlights" => count($this->flights),
+            "cheapestPrice" => $this->cheapestPrice,
+            "cheapestGroup" => $this->cheapestGroup
         );
+    }
+
+    /**
+     * @param $o
+     * @param $i
+     * @return string
+     */
+    private function createUniqueIdgroup($o, $i): string
+    {
+        $uniqueId = implode('', array_column($o, 'id'));
+        $uniqueId .= implode('', array_column($o, 'flightNumber'));
+        $uniqueId .= implode('', array_column($i, 'id'));
+        $uniqueId .= implode('', array_column($i, 'flightNumber'));
+        return base64_encode($uniqueId);
     }
 
 }
